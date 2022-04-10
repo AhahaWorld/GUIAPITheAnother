69fa208787cfe96509c860bfc76883d1fdec5f4a
aruru-weed <aruzenyukkuri@gmail.com>
2022-04-10 16:43:26 +0900

    add Event System

---
+++ src/main/java/info/ahaha/guiapitheanother/GUIEventManager.java
@@ -0,0 +1,64 @@
+package info.ahaha.guiapitheanother;
+
+import info.ahaha.guiapitheanother.annotation.GUIEventHandler;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+
+public class GUIEventManager {
+    private final Map<GUIListener, List<ListenMethod>> guiListenerMethodMap = new HashMap<>();
+
+    public static List<ListenMethod> enumerateListenMethod(GUIListener listener) {
+        List<ListenMethod> list = new ArrayList<>();
+        Class<?> clazz = listener.getClass();
+        for (Method method : clazz.getMethods()) {
+            if (!method.isAnnotationPresent(GUIEventHandler.class))
+                continue;
+            if (method.getParameterCount() != 1)
+                continue;
+            if (!method.getParameterTypes()[0].getGenericSuperclass().equals(GUIEvent.class))
+                continue;
+            list.add(new ListenMethod(method, method.getParameterTypes()[0]));
+        }
+        return list;
+    }
+
+    public List<ListenMethod> register(GUIListener listener) {
+        List<ListenMethod> enumerated = enumerateListenMethod(listener);
+        guiListenerMethodMap.put(listener, enumerated);
+        return enumerated;
+    }
+
+    public Map.Entry<GUIListener, List<ListenMethod>> unregister(GUIListener listener) {
+        Map.Entry<GUIListener, List<ListenMethod>> entry = new AbstractMap.SimpleEntry<>(listener, guiListenerMethodMap.get(listener));
+        guiListenerMethodMap.remove(listener);
+        return entry;
+    }
+
+    public void call(GUIEvent e) {
+        for (GUIListener listener : guiListenerMethodMap.keySet())
+            for (ListenMethod m : guiListenerMethodMap.get(listener))
+                if (m.argEventClass.equals(e.getClass())) {
+                    try {
+                        m.method.invoke(listener, e);
+                    } catch (IllegalAccessException | InvocationTargetException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+    }
+
+    public Map<GUIListener, List<ListenMethod>> getListenersMap() {
+        return guiListenerMethodMap;
+    }
+
+    public static class ListenMethod {
+        public ListenMethod(Method method, Class<?> argEventClass) {
+            this.method = method;
+            this.argEventClass = argEventClass;
+        }
+
+        public Method method;
+        public Class<?> argEventClass;
+    }
+}
---
+++ src/test/java/info/ahaha/guiapitheanother/GUIEventManagerTest.java
@@ -0,0 +1,47 @@
+package info.ahaha.guiapitheanother;
+
+import info.ahaha.guiapitheanother.example.SayYoButtonPushListener;
+import info.ahaha.guiapitheanother.guis.events.ButtonPushEvent;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GUIEventManagerTest {
+    GUIEventManager manager = new GUIEventManager();
+    SayYoButtonPushListener listener = new SayYoButtonPushListener();
+
+    @Test
+    void enumerateListenMethod() {
+        SayYoButtonPushListener listener = new SayYoButtonPushListener();
+        List<GUIEventManager.ListenMethod> enumerated = GUIEventManager.enumerateListenMethod(listener);
+        for(GUIEventManager.ListenMethod m : enumerated)
+            System.out.println(m.method.getName() + " : " + m.argEventClass.getName());
+        assertTrue(enumerated.size() != 0);
+    }
+
+    @Test
+    void main(){
+        register();
+        call();
+        unregister();
+    }
+
+    void register() {
+        List<GUIEventManager.ListenMethod> list = manager.register(listener);
+        assertNotNull(list);
+        assertTrue(list.size() != 0);
+    }
+
+    void call() {
+        manager.call(new ButtonPushEvent(null, new Session(null, null), null));
+    }
+
+    void unregister() {
+        Map.Entry<GUIListener, List<GUIEventManager.ListenMethod>> entry = manager.unregister(listener);
+        assertNotNull(entry);
+        assertTrue(manager.getListenersMap().size() == 0);
+    }
+}
