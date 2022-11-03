7d0ee230d74fb9e99fd25891344cea684218f6ce
kaogon128 <bump_love128@yahoo.co.jp>
2022-11-03 22:52:18 +0900

    TODOの追加といらんもん消した

--- src/main/java/info/ahaha/guiapitheanother/bedrock/SupportedBedrockGUI.java
+++ src/main/java/info/ahaha/guiapitheanother/bedrock/SupportedBedrockGUI.java
@@ -9,7 +9,7 @@ import org.geysermc.floodgate.api.player.FloodgatePlayer;
 
 public interface SupportedBedrockGUI extends GUI {
     default Session show(Player player) {
-        if (FloodgateApi.getInstance().isFloodgatePlayer(player.getUniqueId()) && this instanceof SupportedBedrockGUI) {
+        if (FloodgateApi.getInstance().isFloodgatePlayer(player.getUniqueId())) {
             return this.showForBE(player, FloodgateApi.getInstance().getPlayer(player.getUniqueId()));
         }else{
             return this.showForJE(player);
